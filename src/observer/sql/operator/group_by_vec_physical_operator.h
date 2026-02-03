1f9e891816c582f6b1dc4b72f5c14f38f2e99805
nagisa-kun <1434936049@qq.com>
2026-02-04 01:50:39 +0800

    feat: group by

--- src/observer/sql/operator/group_by_vec_physical_operator.h
+++ src/observer/sql/operator/group_by_vec_physical_operator.h
@@ -11,24 +11,32 @@ See the Mulan PSL v2 for more details. */
 #pragma once
 
 #include "sql/expr/aggregate_hash_table.h"
+#include "sql/operator/group_by_physical_operator.h"
+#include "sql/operator/expr_vec_physical_operator.h"
+
 #include "sql/operator/physical_operator.h"
+#include "storage/common/chunk.h"
 
 /**
  * @brief Group By 物理算子(vectorized)
  * @ingroup PhysicalOperator
  */
-class GroupByVecPhysicalOperator : public PhysicalOperator
+class GroupByVecPhysicalOperator : public GroupByPhysicalOperator
 {
 public:
-  GroupByVecPhysicalOperator(vector<unique_ptr<Expression>> &&group_by_exprs, vector<Expression *> &&expressions){};
+  GroupByVecPhysicalOperator(vector<unique_ptr<Expression>> &&group_by_exprs, vector<Expression *> &&expressions);
 
   virtual ~GroupByVecPhysicalOperator() = default;
 
   PhysicalOperatorType type() const override { return PhysicalOperatorType::GROUP_BY_VEC; }
 
-  RC open(Trx *trx) override { return RC::UNIMPLEMENTED; }
-  RC next(Chunk &chunk) override { return RC::UNIMPLEMENTED; }
-  RC close() override { return RC::UNIMPLEMENTED; }
+  RC open(Trx *trx) override;
+  RC next(Chunk &chunk) override;
+  RC close() override;
 
 private:
+  vector<unique_ptr<Expression>> group_by_exprs_;
+  unique_ptr<AggregateHashTable> aggregate_hash_table_;
+  unique_ptr<AggregateHashTable::Scanner> hash_table_scanner_;
+  Chunk output_chunk_;
 };
